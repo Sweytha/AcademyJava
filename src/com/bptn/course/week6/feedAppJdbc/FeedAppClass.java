8a9f66349937e3c01b6922cb056dc72d314be80c
Sweytha <73808022+Sweytha@users.noreply.github.com>
2023-09-26 14:15:27 -0400

    Update FeedAppClass.java

--- src/com/bptn/course/week6/feedAppJdbc/FeedAppClass.java
+++ src/com/bptn/course/week6/feedAppJdbc/FeedAppClass.java
@@ -6,69 +6,62 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class FeedAppClass {
+    private static final String userName = "postgres";
+    private static final String password = "pg123";
+    private static final String dbName = "feedApp";
+    private static final String port = "5432";
+    private static final String dbUrl = "localhost";
 
-	private static final String userName = "postgres";
-	private static final String password = "pg123";
-	private static final String dbName = "feedApp";
-	private static final String port = "5432";
-	private static final String dbUrl = "localhost";
+    public Connection createConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager
+                .getConnection("jdbc:postgresql://" + dbUrl + ":" + port + "/" + dbName,
+                    userName, password);
 
-	// create a connection
-	public Connection createConnection() {
+        } catch (Exception e) {
+            System.out.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+        return conn;
+    }
 
-		Connection conn = null;
+    
+    public void addUser(Connection conn, String firstName, String lastName, String username, String phone, String emailId, String password, boolean emailVerified) {
+        try {
+            PreparedStatement stmt = null;
+            String sql = "INSERT INTO \"User\" (\"firstName\", \"lastName\", \"username\", \"phone\", \"emailId\", \"password\", \"emailVerified\", \"createdOn\") VALUES (?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)";
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, firstName); 
+            stmt.setString(2, lastName); 
+            stmt.setString(3, username); 
+            stmt.setString(4, phone);
+            stmt.setString(5, emailId);
+            stmt.setString(6, password);
+            stmt.setBoolean(7, emailVerified);
+            
 
-		try {
-			Class.forName("org.postgresql.Driver");
-			conn = DriverManager.getConnection("jdbc:postgresql://" + dbUrl + ":" + port + "/" + dbName, userName,
-					password);
+            int resultCount = stmt.executeUpdate();  
+            System.out.println(resultCount + " record(s) inserted");  
 
-		} catch (Exception e) {
-			System.out.println(e.getClass().getName() + ": " + e.getMessage());
-			System.exit(0);
-		}
-		System.out.println("Opened database successfully");
-		return conn;
+            stmt.close();
+        } catch (SQLException ex) {
+            //handle exception
+            System.out.println("Exception:" + ex.getMessage());
+        }
+    }
 
-	}
-
-	// add user method
-	public void addUser(Connection conn, String firstName, String lastName, String username, String phone,
-			String emailId, String password, boolean emailVerified) {
-
-		try {
-			PreparedStatement stmt = null;
-			String sql = "INSERT INTO \"User\" (\"firstName\", \"lastName\", \"username\", \"phone\", \"emailId\", \"password\", \"emailVerified\", \"createdOn\") VALUES (?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)";
-			stmt = conn.prepareStatement(sql);
-			stmt.setString(1, firstName);
-			stmt.setString(2, lastName);
-			stmt.setString(3, username);
-			stmt.setString(4, phone);
-			stmt.setString(5, emailId);
-			stmt.setString(6, password);
-			stmt.setBoolean(7, emailVerified);
-
-			int resultCount = stmt.executeUpdate();
-			System.out.println(resultCount + " record(s) inserted");
-
-		} catch (SQLException ex) {
-			// handle exception
-			System.out.println("Exception:" + ex.getMessage());
-
-		}
-	}
-	
-	//close the connection
-	public boolean closeConnection(Connection conn) {
-
-		boolean flag = true;
-		try {
-			conn.close();
-		} catch (SQLException ex) {
-			flag = false;
-			System.out.println("Exception: " + ex.getMessage());
-		}
-		return flag;
-
-	}
+    public boolean closeConnection(Connection conn) {
+        boolean flag = true;
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            flag = false;
+            //handle exception
+            System.out.println("Exception:" + ex.getMessage());
+        }
+        return flag;
+    }
 }
